a895627ead78574d3085c023f9cd56817880b1f8
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 17:28:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,15 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string.toLowerCase() === string) 
     return "I can't hear you!";
   } 
 
 
-  if (string === "HELLO") {
-    return "YES INDEED!";
-  }
-}
+
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
