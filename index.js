beaba7dfd624d3e59be2ae2c59f445064dd0b2c6
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-07 17:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) 
     return "I can\'t hear you!";
-  } else if (string.toUpperCase() === string) {
+  } 
+  
+  if (string.toUpperCase() === string) {
      return "YES INDEED!";
   }
+  
+  if (string === )
 
     
 
